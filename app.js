6d8053e3eb751fa408e25075b57713d47f620980
pluderes <kingofpoppro@gmail.com>
2021-10-30 16:29:31 +0700

    feat: add cors

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 // local module
 const { connect } = require("./config/dbConnect");
 const Router = require("./router");
-
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 const POST = process.env.POST || 5001;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
