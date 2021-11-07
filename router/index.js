4f420431c28b88b6bc94cbf0ac8a3862291c437c
pluderes <kingofpoppro@gmail.com>
2021-11-07 20:46:43 +0700

    feat: add route login

--- router/index.js
+++ router/index.js
@@ -7,6 +7,7 @@ const MDW = require("../middlewares/authToken");
 const app = express();
 
 app.use("/user", userRoute);
-app.use("/login", MDW.required, loginRoute);
+app.use("/loginToken", MDW.required, loginRoute);
+app.use("/login", loginRoute);
 app.use("/workspace", workspaceRoute);
 module.exports = app;
