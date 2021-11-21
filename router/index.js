c322c7e59412047377ee900c3efcb50766ef0eb4
pluderes <kingofpoppro@gmail.com>
2021-11-21 19:03:11 +0700

    feat: add route(table, column, card)

--- router/index.js
+++ router/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const userRoute = require("./user.route");
 const loginRoute = require("./login.route");
 const workspaceRoute = require("./workspace.route");
+const tableRoute = require("./table.route");
+const columnRoute = require("./column.route");
+const cardRoute = require("./card.route");
 
 const MDW = require("../middlewares/authToken");
 const app = express();
@@ -10,4 +13,8 @@ app.use("/user", userRoute);
 app.use("/loginToken", MDW.required, loginRoute);
 app.use("/login", loginRoute);
 app.use("/workspace", workspaceRoute);
+app.use("/table", tableRoute);
+app.use("/column", columnRoute);
+app.use("/card", cardRoute);
+
 module.exports = app;
