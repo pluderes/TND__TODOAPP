8dbd95c62a1e053a2716ab414975c6af23e8ec9b
pluderes <kingofpoppro@gmail.com>
2021-11-21 19:07:54 +0700

    feat: add controller table, column, card

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 const User = require("./user.controller");
 const Login = require("./login.controller");
 const Workspace = require("./workspace.controller");
+const Table = require("./table.controller");
+const Column = require("./column.controller");
+const Card = require("./card.controller");
 
 module.exports = {
   User,
   Login,
   Workspace,
+  Table,
+  Column,
+  Card,
 };
